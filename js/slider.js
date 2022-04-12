f54a5779c1c30b2928c0617d382cc2845d2fe530
Drew Paramonov <45173388+odnimslovom@users.noreply.github.com>
2022-04-12 16:59:20 +0300

    bugfix: fix bugs after first review

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,7 @@ sliders.forEach( slider => initSlider(slider));
 
 function initSlider(slider) {
   let sliderBlocks = slider.querySelectorAll('.slider__block');
-  let sliderDots = slider.querySelectorAll('.slider__controls-dot');
+  let sliderDots = slider.querySelectorAll('.button_type_dot');
 
   sliderDots.forEach( (dot, index) => {
     dot.addEventListener('click', () => showBlock(index));
@@ -12,8 +12,8 @@ function initSlider(slider) {
 
   function showBlock(index) {
     sliderBlocks.forEach( sliderBlock => sliderBlock.classList.remove('slider__block_active'));
-    sliderDots.forEach(sliderDot => sliderDot.classList.remove('slider__controls-dot_active'));
+    sliderDots.forEach(sliderDot => sliderDot.classList.remove('button_type_dot-active'));
     sliderBlocks[index].classList.add('slider__block_active');
-    sliderDots[index].classList.add('slider__controls-dot_active');
+    sliderDots[index].classList.add('button_type_dot-active');
   }
 }
